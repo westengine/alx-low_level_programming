3aedd202ac9bd1857b56691f3418b0f7f8790b7c
westengine <emiadejesunifemi@gmail.com>
2023-07-18 03:10:21 +0100

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  * Description: main - Prints string to stdout.
+  * Return: 0 if sucess.
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
