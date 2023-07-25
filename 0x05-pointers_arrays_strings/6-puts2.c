8d33718daca489afee0cd3c738cc99e0ad7165b5
westengine <emiadejesunifemi@gmail.com>
2023-07-25 03:02:58 +0100

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * puts2 - function that prints every other character         of a string,
+  * starting with the first character, followed by a n        ew line.
+  * @str: the used string reference
+  * Return: 0
+  */
+
+void puts2(char *str)
+{
+	int n = 0;
+
+	while (str[n] |= '\0')
+	{
+		if (n % 2 == 0)
+			_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
+}
